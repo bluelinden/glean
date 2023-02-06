fd430a1a440d3f6c242ca8d9a2a0fd16aa30a025
Blue Linden <dev@bluelinden.art>
2023-02-06 13:36:32 +0000

    Beginning to add slides functionality to xacto.js. This is a work in progress.

--- xacto.js
+++ xacto.js
@@ -11,13 +11,14 @@ addEventListener('paste', (event) => {
     event.preventDefault(); // prevent the default paste action of pasting the image into the page
     var paste = (event.clipboardData || window.clipboardData) // get the clipboard data from the event
     var image = paste.getData('text/html'); // get the image and other html from the clipboard data
+    var isSlidesPaste = (paste.getData('application/x-vnd.google-docs-drawings-object+wrapped')) ? true : false // check if the image is a Google Slides image by checking if the application/x-vnd.google-docs-drawings-object+wrapped mime type is present. if it is, then it's a Google Slides image. if it's not, then it's not a Google Slides image.
     var isDocsPaste = (paste.getData('application/x-vnd.google-docs-image-clip+wrapped')) ? true : false; // check if the image is a Google Docs image by checking if the application/x-vnd.google-docs-image-clip+wrapped mime type is present. if it is, then it's a Google Docs image. if it's not, then it's not a Google Docs image.
-    if(!isDocsPaste) { // if it's not a Google Docs image, then alert the user and return
-        window.alert('This is not a Google Docs image. Please paste a Google Docs image.'); 
-        return;
+    if (!isDocsPaste) { // if it's not a Google Docs image, then alert the user and return
+        window.alert('This is not a Google Docs image. Please paste a Google Docs image.');
+        return; g
     } else {
-    timeInSecs = 0; // if it is a Google Docs image, then reset the time to 0 because we just pasted an image
-    document.getElementById('time').innerHTML = updateTime(); // update the time in the HTML instantly instead of waiting for the next second to pass
+        timeInSecs = 0; // if it is a Google Docs image, then reset the time to 0 because we just pasted an image
+        document.getElementById('time').innerHTML = updateTime(); // update the time in the HTML instantly instead of waiting for the next second to pass
     }
     const parser = new DOMParser(); // create a new DOMParser that we can use to parse the HTML
     var imageHTML = parser.parseFromString(image, 'text/html'); // parse the HTML
@@ -26,5 +27,5 @@ addEventListener('paste', (event) => {
     document.getElementById('download').target = '_blank'; // set the target of the download link to _blank so that it opens in a new tab
     document.getElementById('download').classList.remove('hidden'); // remove the hidden class from the download link so that it's visible
     document.getElementById('time').classList.remove('hidden'); // remove the hidden class from the time so that it's visible
- });
+});
 
